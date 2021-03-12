5f932f3b2a12a978b462416a03c1407472f5edaf
Bruno Martins <bruno.martins217@gmail.com>
2021-03-12 16:50:19 -0300

    Adicionando src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      text: '',
+      pokemon: [],
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  async handleClick(text)  {
+    const data= await fetch(`https://api.pokemontcg.io/v1/cards?name=${text}`)
+    const poke = await data.json()
+    this.setState({
+      pokemon: poke.cards
+    })
+  }
+
+  handleChange(e) {
+    this.setState({
+      text: e.target.value,
+    })
+  }
+
+  render() {
+    const { text, pokemon } = this.state
+    return (
+      <div className="main">
+        <header className="App-header">
+          <h1>Pokemon</h1>
+        <nav>
+          <label htmlFor="input">
+            <input onChange={ (e) => this.handleChange(e) }name="input" type="text"/>
+            <button onClick={() => this.handleClick(text)} type="button">Search</button>
+          </label>
+        </nav> 
+        </header> 
+        <main className="App-cards">
+          {pokemon.map((item) => (
+            <div className="card">
+              <h3 className="card-title" key={item.id}>{item.name}</h3>
+              <img src={item.imageUrl} alt={item.name}/>
+              <p className="card-type" key={item.number}>{item.types}</p>
+            </div>
+              ))}
+        </main>      
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
